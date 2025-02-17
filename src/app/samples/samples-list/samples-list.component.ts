504944f08044e0d61a5ed709971237dbbaa13eb2
Sascha Mundstein <mundstein@gmail.com>
2025-02-17 13:58:59 +0100

    Working on sample list component input and output.

--- src/app/samples/samples-list/samples-list.component.ts
+++ src/app/samples/samples-list/samples-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DataService } from '../../api/data.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-samples-list',
@@ -15,27 +16,54 @@ export class SamplesListComponent implements OnInit {
   samples$: Observable<any[]>;
   selectionEnabled = false
   selectedSamples: any[] = []
-
+  
+  @Output() sampleSelected = new EventEmitter<any>();
+  
   /* By specifiying the link input, you can change the link 
-   that is used to navigate to the detail page. */
+  that is used to navigate to the detail page or manage selections. */
   @Input() link = 'detail'
-
+  errorMessage = ''
+  
   getLink(sample: any): string {
-    return this.link === 'detail' ? `/samples/${sample.id}` : `/phrases/${sample.id}`;
+    if (this.link == 'detail') {
+      return `/samples/${sample.id}`
+    } else if (this.link == 'phrases') {
+      return `/phrases/${sample.id}`
+    } else if (this.link == 'select') {
+      return ''
+    }
+    return ''
   }
-
+  
   constructor(
     private dataService: DataService,
     private route: ActivatedRoute,
   ) { 
     this.samples$ = this.dataService.getSamples().pipe(
-      map(samples => samples.map((sample: any) => ({
-      ...sample,
-      dialect_name: sample.dialect_name ? sample.dialect_name : '(No Dialect Specified)'
-      })))
+      catchError(error => {
+        console.error('Error retrieving samples', error);
+        this.errorMessage = error.message;
+        return of([]);
+      })
     );
+    this.selectionEnabled = this.link == 'select'
+  }
+  
+  ngOnInit(): void {
   }
 
-  ngOnInit(): void {
+  onSampleSelect(sample: any): void {
+    sample.selected=!sample.selected;
+    this.sampleSelected.emit(sample)
+  }
+
+  updateSelected(selectedSamples: any[]) {
+    if (this.samples$) {
+      this.samples$.subscribe(samples => {
+        samples.forEach(sample => {
+          sample.selected = selectedSamples.some(s => s.sample_ref == sample.sample_ref)
+        })
+      })    
+    }
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SamplesListComponent } from "../samples/samples-list/samples-list.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [SamplesListComponent, CommonModule, FormsModule,],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
 
+  @ViewChild(SamplesListComponent) samplesListComponent!: SamplesListComponent;
+  selectedSamples: any[] = [];
+
+  onSampleSelected(sample: any): void {
+    console.log(sample.selected ? 'Sample selected' : 'Sample unselected', sample)
+    if (sample.selected) {
+      this.selectedSamples.push(sample)
+    } else {
+      this.selectedSamples = this.selectedSamples.filter(s => s.sample_ref != sample.sample_ref)
+    }
+  }
+  
+  deselect(sample:any) {
+    this.selectedSamples = this.selectedSamples.filter(s => s.sample_ref != sample.sample_ref)
+    if (this.samplesListComponent) {
+      this.samplesListComponent.updateSelected(this.selectedSamples);
+    }
+  }
 }
+
