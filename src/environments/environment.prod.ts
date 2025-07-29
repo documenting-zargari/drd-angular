183787c0e453b2dd9368c99f468f582e04e801fa
ACDH <105779568+acdh@users.noreply.github.com>
2025-07-29 22:57:22 +0200

    Delete src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-
-export const environment = {
-   production: true,
-   countryApiToken: "ee2ddfd5b411648d531b959f0b4975c2e5d9b95a",
-   countryApiUrl: "https://aaapis.com/api/v1/info/country/",
-   apiUrl: "http://localhost:8000",
-   audioUrl: "/mp3"
-};
