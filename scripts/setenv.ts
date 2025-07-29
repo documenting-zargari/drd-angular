f1c61306671719438466a447eac58b4e7754d4a8
ACDH <105779568+acdh@users.noreply.github.com>
2025-07-29 20:29:45 +0200

    Update setenv.ts

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -23,6 +23,7 @@ export const environment = {
    countryApiToken: "${process.env['COUNTRY_API_TOKEN']}",
    countryApiUrl: "https://aaapis.com/api/v1/info/country/",
    apiUrl: "${process.env['API_URL']}",
+   apiUrl: "${process.env['AUDIO_URL']}"
 };
 `;
 
