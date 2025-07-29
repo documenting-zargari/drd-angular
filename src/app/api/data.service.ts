f0415fa8853a6a17aabfcdc5e8e9a5ed1acbd7c2
ACDH <105779568+acdh@users.noreply.github.com>
2025-07-29 22:14:00 +0200

    Update data.service.ts

--- src/app/api/data.service.ts
+++ src/app/api/data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 @Injectable({
   providedIn: 'root'
 })
